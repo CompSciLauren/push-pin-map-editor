d51b3b034bb539566579abe2cdb8971f1677e2ab
Lauren Stephenson <CompSciLauren@gmail.com>
2020-08-19 16:28:26 -0500

    Add map as background image of canvas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "react-dom";
-import { Stage, Layer, Star, Text } from "react-konva";
+import { Stage, Layer, Star, Text, Image } from "react-konva";
+import MapOfOverlandPark from "./images/large-detailed-map-of-overland-park.jpg";
 import "./App.css";
 
 function generateShapes() {
@@ -40,36 +40,44 @@ function App() {
     );
   };
 
+  // const imageBitmapPromise = createImageBitmap(MapOfOverlandPark);
+
+  const image = new window.Image();
+  image.src =
+    process.env.PUBLIC_URL + "/large-detailed-map-of-overland-park.jpg";
+
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Text text="Try to drag a star" />
-        {stars.map((star) => (
-          <Star
-            key={star.id}
-            id={star.id}
-            x={star.x}
-            y={star.y}
-            numPoints={5}
-            innerRadius={20}
-            outerRadius={40}
-            fill="#89b717"
-            opacity={0.8}
-            draggable
-            rotation={star.rotation}
-            shadowColor="black"
-            shadowBlur={10}
-            shadowOpacity={0.6}
-            shadowOffsetX={star.isDragging ? 10 : 5}
-            shadowOffsetY={star.isDragging ? 10 : 5}
-            scaleX={star.isDragging ? 1.2 : 1}
-            scaleY={star.isDragging ? 1.2 : 1}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-          />
-        ))}
-      </Layer>
-    </Stage>
+    <div>
+      <Stage width={3409} height={4699}>
+        <Layer>
+          <Image image={image} />
+          {stars.map((star) => (
+            <Star
+              key={star.id}
+              id={star.id}
+              x={star.x}
+              y={star.y}
+              numPoints={5}
+              innerRadius={20}
+              outerRadius={40}
+              fill="#89b717"
+              opacity={0.8}
+              draggable
+              rotation={star.rotation}
+              shadowColor="black"
+              shadowBlur={10}
+              shadowOpacity={0.6}
+              shadowOffsetX={star.isDragging ? 10 : 5}
+              shadowOffsetY={star.isDragging ? 10 : 5}
+              scaleX={star.isDragging ? 1.2 : 1}
+              scaleY={star.isDragging ? 1.2 : 1}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            />
+          ))}
+        </Layer>
+      </Stage>
+    </div>
   );
 }
 
